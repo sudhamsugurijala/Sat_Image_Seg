b711c94784b77ec5b642cfae4c2fa19810746c0a
sudhamsu <sudhamsugurijala@gmail.com>
2021-04-08 21:10:36 +0530

    loading feature added

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import os
 import uuid
 
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request, make_response
 from utils.run_model import getBaseMap
-
 
 app=Flask(__name__)
 
@@ -11,12 +10,12 @@ app=Flask(__name__)
 def index():
     return render_template("upload.html")
 
-@app.route("/upload",methods=['POST'])
-def upload():
+
+@app.route('/loading', methods=['POST'])
+def loading():
     target='./static'
     if not os.path.isdir(target):
         os.mkdir(target)
-
     else :
         # remove older images
         for fname in os.listdir(target):
@@ -28,15 +27,31 @@ def upload():
         dest=os.path.join(target, filename)
         f.save(dest)
 
-    # save basemap to static as map_token.png
-    # token is a 6 digit unique id for each map output (browser cache solution)
-    token = uuid.uuid4().hex[:6] 
-    resp = getBaseMap(token)
-    if type(resp) == str:
-        return resp
+    return render_template('loading.html')
 
-    filename = "map_{}.png".format(token)
-    return render_template("complete.html" , image_name=filename)
+
+@app.route("/upload", methods=['GET', 'POST'])
+def upload():
+    if request.method=='GET':
+        filename = None
+        # file used for storing base map name between requests
+        with open("./static/filename.txt", 'r') as f:
+            filename = f.read()
+        return render_template("complete.html" , image_name=filename)
+
+    if request.method=='POST':
+        # save basemap to static as map_token.png
+        # token is a 6 digit unique id for each map output (browser cache solution)
+        token = uuid.uuid4().hex[:6] 
+        resp = getBaseMap(token)
+        if type(resp) == str:
+            return resp
+
+        filename = "map_{}.png".format(token)
+        # file used for storing base map name between requests
+        with open("./static/filename.txt", 'w') as f:
+            f.write(filename)
+        return make_response('POST request successful', 200)
     
 if __name__=="__main__":
     app.run(debug=True)
